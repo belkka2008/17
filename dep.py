d18f7097a04c17b70d64ddca401c74995a5048c6
belkka2008 <belkka2008@mail.ru>
2023-05-03 01:15:45 +0300

    Initial commit

---
+++ dep.py
@@ -0,0 +1,14 @@
+money = int (input('Введите сумму:'))
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+A = per_cent.get('ТКБ')
+Bank1 = money*A/100
+B = per_cent.get('СКБ')
+Bank2 = money*B/100
+C = per_cent.get('ВТБ')
+Bank3 = money*C/100
+D = per_cent.get('СБЕР')
+Bank4 = money*D/100
+deposit = [Bank1, Bank2, Bank3, Bank4]
+deposit.sort()
+print(list(map(int, deposit)))
+print('Максимальная сумма, которую вы можете заработать -',deposit[-1])
